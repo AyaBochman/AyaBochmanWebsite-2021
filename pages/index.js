c5c828a5ad08377431009fe563049ffeb3172c63
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-04-27 21:31:18 +0200

    added content and about wip

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Head from 'next/head'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Content from '../components/Content';
+import styled from 'styled-components';
 
 export default function Home() {
   return (
-    <div>
+    <StyledHome>
       <Head>
         <title>Aya Bochman | Full Stack Web Developer</title>
         <link rel="icon" href="/laptop.ico" />
       </Head>
 
       <Header />
-      <main className={styles.main}>
+      <Navbar />
+      <Content />
+      {/* <main className={styles.main}>
 
-      </main>
+      </main> */}
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
 
-      </footer>
-    </div>
+      </footer> */}
+    </StyledHome>
   )
 }
+
+const StyledHome = styled.div`
+
+`;
