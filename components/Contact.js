4b32642dccb59318fe81192803c2a1a65bc03e87
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-05-04 19:06:52 +0200

    effect to email btn

--- components/Contact.js
+++ components/Contact.js
@@ -10,13 +10,12 @@ const Contact = () => {
         <StyledContact>
             <Heading text={'contact'} color={'#fff'} />
             <div className={'contact-details'}>
+            <span><a href='https://www.linkedin.com/in/aya-bochman/' target='_blank'><FontAwesomeIcon icon={faLinkedin} /></a>Let's be friends 🤝</span>
                 <span className={'email'}><FontAwesomeIcon icon={faEnvelope} />aya.bochman@gmail.com</span>
-                <button type="button" className={'send-btn'}><a className={'link-btn'} href={'mailto:aya.bochman@gmail.com'}>Send Email</a><FontAwesomeIcon icon={faPaperPlane} /></button>
+                <button type="button" className={'send-btn'}><a className={'link-btn'} href={'mailto:aya.bochman@gmail.com'}>Send Email</a><FontAwesomeIcon className={'paper-plane'} icon={faPaperPlane} /></button>
                 <div className={'links-container'}>
-                    <span><a href='https://www.linkedin.com/in/aya-bochman/' target='_blank'><FontAwesomeIcon icon={faLinkedin} /></a>Let's be friends 🤝</span>
-                    <span><a href='https://github.com/AyaBochman' target='_blank'><FontAwesomeIcon icon={faGithub} /></a>Github to this page :)</span>
+                    <span><a href='https://github.com/AyaBochman' target='_blank'><FontAwesomeIcon icon={faGithub} /></a></span>
                 </div>
-
             </div>
 
         </StyledContact>
@@ -41,10 +40,13 @@ line-height: 2;
 svg{
     font-size: 28px;
     transition: all .1s ease-in-out;
+    margin-right: 10px;
 &:hover{
-  transform: scale(1.2);
+    transform: scale(1.2);
 }
-margin-right: 10px;
+.paper-plane:hover{
+     transform: none;   
+    }
 }
 .email{
     display: flex;
@@ -52,20 +54,39 @@ margin-right: 10px;
     justify-content:center;
 }
 .send-btn{
-text-transform: uppercase;
-font-size: 20px;
-height: 50px;
-display: flex;
-text-align: center;
-margin: 10px auto;
-align-items: center;
-border-radius: 10px;
-color: #fff;
-background: var(--gradient-1);
-border: none;
+    position: relative;
+    text-transform: uppercase;
+    font-size: 20px;
+    height: 50px;
+    width: auto;
+    display: flex;
+    text-align: center;
+    margin: 10px auto;
+    align-items: center;
+    border-radius: 10px;
+    color: #fff;
+    background: var(--gradient-1);
+    border: none;
 a{
     margin-right: 5px;
 }
+}
+.send-btn:after{
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    background-color: rgba(255,255,255,0.4);
+    transition: none;
+    height: 100%;
+}
+.send-btn:hover:after{
+    cursor: pointer;
+    width: 120%;
+    background-color: rgba(255,255,255,0);
+    transition: all 0.4s ease-in-out;
+
 }
 .links-container{
     display: flex;
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <Navbar />
       <Main />
       {/* <footer> */}
-        <Footer />
+      <Footer />
       {/* </footer> */}
     </StyledHome>
   )
