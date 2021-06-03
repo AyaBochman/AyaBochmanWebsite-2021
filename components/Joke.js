3f569ebaa1874254ce4f1b0b6bec06e05f5844a3
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-06-03 14:39:45 +0200

    added joke, updated header style

---
+++ components/Joke.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Joke = () => {
+  const [joke, setJoke] = useState({
+    setup: "What's the object-oriented way to become wealthy?",
+    delivery: "Inheritance.",
+  });
+  useEffect(() => {
+    const fetchJoke = async () => {
+      const result = await axios(
+        "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist&type=twopart"
+      );
+      if (result && result.data) {
+        setJoke(result.data);
+      }
+    };
+    fetchJoke();
+  }, []);
+
+  return (
+    <JokeStyle>
+      {/* <button type="button">open me</button> */}
+      {
+        <div className={"joke"}>
+          <div className={"joke-head"}>Geek joke of the day:</div>
+          <div className={"joke-text"}>
+            <p style={{ marginTop: "5px" }}>Q: "{joke.setup}"</p>
+            <p>A: "{joke.delivery}"</p>
+            <span className={"joke-credit"}>
+              &nbsp;via&nbsp;
+              <a href={"https://sv443.net/jokeapi/v2/"}>JokeApi</a>
+            </span>
+          </div>
+        </div>
+      }
+    </JokeStyle>
+  );
+};
+
+export default Joke;
+
+const JokeStyle = styled.div`
+background-color: var(--gradient-2);
+display: flex;
+color: #fff;
+padding: 25px;
+box-shadow: 0 4px 6px -4px grey;
+.joke{
+    margin: 0 auto;
+}
+.joke-head{
+    font-family: 'Courgette', cursive;
+    color: var(--gradient-1);
+    font-size: 28px;
+    text-align: center;
+}
+.joke-text{
+    line-height: 1.2;
+    padding: 10px 0;
+}
+  .joke-credit {
+    font-size: 12px;
+    float: right;
+    a {
+      text-decoration: underline;
+    }
+  }
+`;
