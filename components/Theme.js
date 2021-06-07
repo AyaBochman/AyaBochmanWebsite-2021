c9929c39a1af3decb4bf7ee93d6d9ca314060d3a
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-06-07 19:20:58 +0200

    added darkmode

---
+++ components/Theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+    body: 'var(--light-bg)',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: 'var(--light-bg)',
+    link: 'green'
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
---
+++ components/commons/CustomButton.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const CustomButton = ({ text, icon }) => {
+  return (
+    <StyledButton>
+      <button type="button" className={"send-btn"}>
+        <span className={"text"}>{text}</span>
+        <span className={"icon"}>{icon && icon}</span>
+      </button>
+    </StyledButton>
+  );
+};
+
+export default CustomButton;
+
+const StyledButton = styled.div`
+  .send-btn {
+    position: relative;
+    text-transform: uppercase;
+    font-size: 18px;
+    height: 50px;
+    width: auto;
+    display: flex;
+    text-align: center;
+    margin: 10px auto;
+    align-items: center;
+    border-radius: 10px;
+    color: #fff;
+    background: var(--gradient-1);
+    border: none;
+    padding: 0 15px;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .send-btn:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    background-color: rgba(255, 255, 255, 0.4);
+    transition: none;
+    height: 100%;
+  }
+  .send-btn:hover:after {
+    width: 120%;
+    background-color: rgba(255, 255, 255, 0);
+    transition: all 0.5s ease-in-out;
+  }
+  .text{
+      margin-right: 5px;
+  }
+  .icon{
+    height: 24px;
+    width: 24px;
+  }
+`;
